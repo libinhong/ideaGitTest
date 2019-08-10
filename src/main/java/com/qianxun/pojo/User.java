d0c881de36f3b47159bc605b7d3d52f3b8ccea5b
qianxun <qianxun@qq.com>
2019-08-10 20:19:07 +0800

    修改User

--- src/main/java/com/qianxun/pojo/User.java
+++ src/main/java/com/qianxun/pojo/User.java
@@ -3,15 +3,15 @@ package com.qianxun.pojo;
 public class User {
     private String username;
     private String password;
-    private String email;
+
 
     public User() {
     }
 
-    public User(String username, String password, String email) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
+
     }
 
     public String getUsername() {
@@ -30,11 +30,4 @@ public class User {
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
